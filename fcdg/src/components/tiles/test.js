0e9b2b3ed33dea1fb6340742cb68dfaaaa76c8d3
unknown <Darien.stefani@Darien-ITW10PRO.domain.clovis.anlin.com>
2021-09-10 12:33:50 -0700

    updated cards

--- fcdg/src/components/tiles/test.js
+++ fcdg/src/components/tiles/test.js
@@ -4,57 +4,87 @@ import './Tiles.css';
 import Card from 'react-bootstrap/Card'
 import { Col, Row} from "react-bootstrap";
 import CardGroup from 'react-bootstrap/CardGroup'
+import "../../assets/img/etc/I1.jpg"
+
+
 
 function Test(){
     return (
 <div>
-<CardGroup>
-  <Card class="card">
-    <Card.Img class="image" variant="top" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
-    <Card.Body>
-      <Card.Title class="title">Card title</Card.Title>
-      <Card.Text class="description">
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card class="card">
-    <Card.Img class="image" variant="top" src="https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW5kaWF8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" />
-    <Card.Body>
-      <Card.Title class="title">Card title</Card.Title>
-      <Card.Text class="description">
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card class="card">
-    <Card.Img class="image" variant="top" src="https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg" />
-    <Card.Body>
-      <Card.Title class="title">Card title</Card.Title>
-      <Card.Text class="description">
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-</CardGroup>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <img src="https://c.tenor.com/hpyQ-qg72MEAAAAM/mocking-chicken.gif" width="150" height="150" class="card-img-top"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="https://media.tenor.com/images/ad9785e577e256febefa9472c755569f/tenor.gif" width="150" height="150" class="card-img-top"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="https://i.pinimg.com/originals/6e/7b/73/6e7b73359877f40b3f1ab8229d830466.gif" width="150" height="150" class="card-img-top"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <img src="https://c.tenor.com/hpyQ-qg72MEAAAAM/mocking-chicken.gif" width="150" height="150" class="card-img-top"></img>
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="https://media.tenor.com/images/ad9785e577e256febefa9472c755569f/tenor.gif" width="150" height="150" class="card-img-top"></img>
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src={require('../../assets/img/etc/I1.jpg')} width="150" height="150" class="card-img-top"></img>
+                    <div class="card-body">
+                        <h5 class="card-title">Card Title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 </div>
-
-
 
     )
 }
