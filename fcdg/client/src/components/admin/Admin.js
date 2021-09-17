bb21d8d3ec0a095cbe524fbfaf7571825c1565a3
AnlinDevelopment <licenses@anlin.com>
2021-09-17 15:25:36 -0700

    the admin page can now add users to the database

--- fcdg/client/src/components/admin/Admin.js
+++ fcdg/client/src/components/admin/Admin.js
@@ -1,22 +1,65 @@
 import "./Admin.css";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function Admin() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //function to create a new user and submit it to the database.
+  //These variables pass to server/index.js to the post cmd
+  const submitNewUser = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      BEfirstName: firstName,
+      BElastName: lastName,
+      BEemail: email,
+      BEpassword: password,
+    }).then(() => {
+      alert("Successful Insert");
+    });
+  };
+
   return (
     <div id="Admin-Main" className="admin">
       <title>FCDG Administration</title>
       <div className="form">
         <label>First Name: </label>
-        <input type="text" name="FirstName" />
+        <input
+          type="text"
+          name="FirstName"
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+        />
         <label>Last Name: </label>
-        <input type="text" name="LastName" />
-        <label>Username: </label>
-        <input type="text" name="Username" />
+        <input
+          type="text"
+          name="LastName"
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+        />
         <label>Email: </label>
-        <input type="text" name="Email" />
+        <input
+          type="text"
+          name="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
         <label>Password: </label>
-        <input type="text" name="Password" />
+        <input
+          type="text"
+          name="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
 
-        <button>Submit</button>
+        <button onClick={submitNewUser}>Submit</button>
       </div>
     </div>
   );
--- fcdg/server/index.js
+++ fcdg/server/index.js
@@ -1,9 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { dbTest } from "./components/dbConfig/query";
+const express = require("express");
+const app = express();
+const mariadb = require("mariadb");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-ReactDOM.render(
-  <div>
-    <dbTest />
-  </div>
-);
+//create the database connection to be used later to query the databse.
+const db = mariadb.createPool({
+  host: "192.168.252.199",
+  port: "3306",
+  user: "root",
+  password: "Anlin",
+  database: "fcdg",
+});
+
+//allows us to read and write from the frontend to the backend
+app.use(cors());
+
+//apply the middlewere to grab the json from the front end.
+app.use(express.json());
+
+//will encode everything as jason.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Creates a url so that we can pass variables between the frontend and backend and write to the DB
+app.post("/api/insert", (req, res) => {
+  const BEfirstName = req.body.BEfirstName;
+  const BElastName = req.body.BElastName;
+  const BEemail = req.body.BEemail;
+  const BEpassword = req.body.BEpassword;
+
+  const sqlInsert =
+    "INSERT INTO fcdg.`security` (FirstName, LastName, Email, PW) VALUES (?, ?, ?, ?);";
+
+  db.query(
+    sqlInsert,
+    [BEfirstName, BElastName, BEemail, BEpassword],
+    (err, result) => {
+      console.log("Results: ", result);
+    }
+  );
+});
+
+//make sure the server is running.
+app.listen(3001, () => {
+  console.log("Running on port 3001");
+});
